a29a39eaa08b5b8cf76cd6226e0e02aeec46c33d
Emil Melnikov <emilmelnikov@gmail.com>
2017-02-02 20:14:56 +0300

    Add Doxygen support

--- include/ametsuchi/Greeter.h
+++ include/ametsuchi/Greeter.h
@@ -3,9 +3,17 @@
 #include <string>
 
 namespace ametsuchi {
+/// Greets it's caller.
+/// This class tries to determine caller's psycological profile
+/// using multiple heuristics.
 class Greeter {
  public:
+  /// Creates the greeter and calculates psychological profile.
+  /// \param subject who to greet
   Greeter(const char* subject) : subject(subject) {}
+
+  /// Greets the caller.
+  /// \return a warming welcome message
   std::string greet() const;
 
  private:
