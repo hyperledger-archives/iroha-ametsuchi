fbc900c432ba14c9f88113d4c27082805fa7be47
Bogdan Vaneev <warchantua@gmail.com>
2017-02-08 01:45:02 +0300

    Added option AMETSUCHI_COMPILE_SCHEMA

--- dependencies.cmake
+++ dependencies.cmake
@@ -11,8 +11,8 @@ set_directory_properties(PROPERTIES
 #         flatbuffers          #
 ################################
 ExternalProject_Add(flatbuffers
-  GIT_REPOSITORY "https://github.com/google/flatbuffers.git"
-  GIT_TAG "v1.5.0"
+  GIT_REPOSITORY    "https://github.com/google/flatbuffers.git"
+  GIT_TAG           "v1.5.0"
   CONFIGURE_COMMAND cmake ${EP_PREFIX}/src/flatbuffers -DFLATBUFFERS_BUILD_TESTS=Off -DFLATBUFFERS_INSTALL=Off -DFLATBUFFERS_BUILD_FLATHASH=Off
   INSTALL_COMMAND   "" # remove install step
   TEST_COMMAND      "" # remove test step
@@ -24,27 +24,12 @@ ExternalProject_Get_Property(flatbuffers binary_dir)
 set(flatbuffers_BINARY_DIR "${binary_dir}")
 
 
-### Disable schema autogeneration
-# Set active scema
-#SET(ACTIVE_SCHEMA_NAME "api.fbs")
-#SET(ACTIVE_SCHEMA ${PROJECT_SOURCE_DIR}/schema/${ACTIVE_SCHEMA_NAME})
-#
-#add_custom_command(
-#  TARGET flatbuffers
-#  POST_BUILD
-#  #OUTPUT ${PROJECT_SOURCE_DIR}/include/ametsuchi/api_generated.h
-#  COMMAND ${flatbuffers_BINARY_DIR}/flatc --cpp --scoped-enums ${ACTIVE_SCHEMA}
-#  WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/include/ametsuchi # includes will be in this directory
-#  COMMENT "Generating schema"
-#)
-
-
 #############################
 #         speedlog          #
 #############################
 ExternalProject_Add(spdlog
-  GIT_REPOSITORY "https://github.com/gabime/spdlog.git"
-  GIT_TAG "v0.11.0"
+  GIT_REPOSITORY    "https://github.com/gabime/spdlog.git"
+  GIT_TAG           "v0.11.0"
   INSTALL_COMMAND   "" # remove install step
   TEST_COMMAND      "" # remove test step
   UPDATE_COMMAND    "" # remove update step
@@ -58,24 +43,15 @@ set(spdlog_SOURCE_DIR "${source_dir}")
 #         keccak          #
 ###########################
 ExternalProject_Add(keccak
-  GIT_REPOSITORY "https://github.com/gvanas/KeccakCodePackage.git"
+  GIT_REPOSITORY    "https://github.com/gvanas/KeccakCodePackage.git"
   CONFIGURE_COMMAND ""
-  BUILD_IN_SOURCE 1
-  BUILD_COMMAND     make -j4 && make -j4 generic64/libkeccak.a
+  BUILD_IN_SOURCE   1
+  BUILD_COMMAND     make && make generic64/libkeccak.a
   INSTALL_COMMAND   "" # remove install step
   TEST_COMMAND      "" # remove test step
   UPDATE_COMMAND    "" # remove update step
 )
 ExternalProject_Get_Property(keccak source_dir)
 set(keccak_SOURCE_DIR  "${source_dir}")
-set(keccak_INCLUDE_DIR ${keccak_SOURCE_DIR}/bin/generic64)
+set(keccak_INCLUDE_DIR ${keccak_SOURCE_DIR}/bin/generic64/libkeccak.a.headers)
 set(keccak_LINK_DIR    ${keccak_SOURCE_DIR}/bin/generic64)
-
-# it is used to rename libkeccak.a.headers to keccak. 
-# we want namespaced include: #include <keccak/blablasha.h>
-add_custom_command(
-  TARGET keccak
-  POST_BUILD
-  COMMAND mv ${keccak_LINK_DIR}/libkeccak.a.headers ${keccak_LINK_DIR}/keccak
-  COMMENT "mv libkeccak.a.headers keccak"
-)
