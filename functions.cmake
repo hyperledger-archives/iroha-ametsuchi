5538ecff9410d0cac720cf4e9977f3453f821471
Bogdan Vaneev <warchantua@gmail.com>
2017-02-14 19:01:41 +0300

    Fixed error in circle ci

--- functions.cmake
+++ functions.cmake
@@ -26,7 +26,7 @@ function(StrictMode target)
 endfunction()
 
 
-# Creates test "test_name", with "SOURCES"(use string as second argument)
+# Creates test "test_name", with "SOURCES" (use string as second argument)
 function(AddTest test_name SOURCES)
   list(APPEND SOURCES main.cc)
   add_executable(${test_name} ${SOURCES})
@@ -38,4 +38,14 @@ function(AddTest test_name SOURCES)
     COMMAND $<TARGET_FILE:${test_name}>
   )
   StrictMode(${test_name})
+endfunction()
+
+
+# Creates benchmark "bench_name", with "SOURCES" (use string as second argument)
+function(AddBenchmark bench_name SOURCES)
+  add_executable(${bench_name} ${SOURCES})
+  add_dependencies(${bench_name} gbenchmark)
+  target_include_directories(${bench_name} PRIVATE ${gbenchmark_INCLUDE_DIR})
+  target_link_libraries(${bench_name} PRIVATE ${gtest_LIBRARIES} pthread)
+  StrictMode(${bench_name})
 endfunction()
