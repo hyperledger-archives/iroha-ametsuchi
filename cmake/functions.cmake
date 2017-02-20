796cf754bb89ad654b507cb020fb372cd144906e
Emil Melnikov <emilmelnikov@gmail.com>
2017-02-20 21:00:27 +0300

    Move compiler identification to CMake StrictMode

--- cmake/functions.cmake
+++ cmake/functions.cmake
@@ -5,23 +5,17 @@ function(StrictMode target)
     CXX_STANDARD 14
     CXX_STANDARD_REQUIRED ON
     CXX_EXTENSIONS OFF
-    )
+  )
   # Enable more warnings and turn them into compile errors.
-  if(CMAKE_COMPILER_IS_GNUCXX OR CMAKE_COMPILER_IS_CLANGCXX)
-    target_compile_options(${target} PRIVATE
-      -Wall
-      -Wextra
-      -Werror
-      )
-  elseif(CMAKE_COMPILER_IS_MSVCCXX OR CMAKE_COMPILER_IS_INTELCXX)
-    target_compile_options(${target} PRIVATE
-      /W3
-      /WX
-      )
+  if((CMAKE_CXX_COMPILER_ID STREQUAL "GCC") OR
+     (CMAKE_CXX_COMPILER_ID STREQUAL "Clang") OR
+     (CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang"))
+    target_compile_options(${target} PRIVATE -Wall -Wextra -Werror)
+  elseif((CMAKE_CXX_COMPILER_ID STREQUAL "MSVC") OR
+         (CMAKE_CXX_COMPILER_ID STREQUAL "Intel"))
+    target_compile_options(${target} PRIVATE /W3 /WX)
   else()
-    message(AUTHOR_WARNING
-      "Unknown compiler: building target ${target} with default options"
-      )
+    message(AUTHOR_WARNING "Unknown compiler: building target ${target} with default options")
   endif()
 endfunction()
 
