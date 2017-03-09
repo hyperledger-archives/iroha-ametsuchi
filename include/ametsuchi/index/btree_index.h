45f622e2facf7a6eaaf3de103b3ac7d286ce5f6c
grimadas <bulat.nasrulin@gmail.com>
2017-03-09 09:17:26 +0300

    Code reformat

--- include/ametsuchi/index/btree_index.h
+++ include/ametsuchi/index/btree_index.h
@@ -1,6 +1,19 @@
-//
-// Created by hdalpha on 08.03.17.
-//
+/**
+ * Copyright Soramitsu Co., Ltd. 2016 All Rights Reserved.
+ * http://soramitsu.co.jp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #ifndef LIBLMDB_BTREE_INDEX_H
 #define LIBLMDB_BTREE_INDEX_H
@@ -8,26 +21,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "lmdb.h"
 #include <string>
+#include "lmdb.h"
 
+namespace ametsuchi {
+namespace index {
 
 template <typename K, typename V>
-class btree_index {
+class BTreeIndex {
 
-    MDB_env *env;
-    MDB_dbi dbi;
-    MDB_stat mst;
+  MDB_env *env;
+  MDB_dbi dbi;
+  MDB_stat mst;
 
-public:
-    btree_index(const std::string& fname);
+ public:
+  btree_index(const std::string &fname);
 
-    int insert(K&& key, V&& val);
+  int insert(K &&key, V &&val);
 
-    V get(K&& keyid);
+  V get(K &&keyid);
 
-    ~btree_index();
+  ~btree_index();
 };
 
 
-#endif //LIBLMDB_BTREE_INDEX_H
+}  // namespace index
+}  // namespace ametsuchi
+
+
+#endif  // LIBLMDB_BTREE_INDEX_H
--- src/ametsuchi/index/btree_index.cpp
+++ src/ametsuchi/index/btree_index.cpp
@@ -3,76 +3,77 @@
 //
 
 #include "btree_index.h"
+namespace ametsuchi {
+namespace index {
 
 
-
-btree_index::btree_index(const std::string& fname) {
-
-
-    //TODO: Handle errors
-    mdb_env_create(&env);
-    mdb_env_set_maxreaders(env, 1);
-    mdb_env_set_mapsize(env, 10485760);
-    mdb_env_open(env, fname.c_str(), MDB_FIXEDMAP, 0664);
-
-
-
-
-    // MDB_MAPASYNC - acync write tot disk, can lose last transaction during a system crash
-    // MDB_NORDAHEAD - no read ahead, os should care itself
-    // MDB_WRITEMAP
-    // MDB_FIXEDMAP
-  /*  catch()
-    {
-        mdb_env_close(env);
-    }*/
+template <typename K, typename V>
+btree_index<K, V>::btree_index(const std::string &fname) {
+  mdb_env_create(&env);
+  mdb_env_set_maxreaders(env, 1);
+  mdb_env_set_mapsize(env, 10485760);
+  int res = mdb_env_open(env, fname.c_str(), MDB_FIXEDMAP, 0664);
+  if (res) {
+    printf("res is not NULL when opening ");
+    exit(-1);
+  }
+}
 
 
+template <typename K, typename V>
+V btree_index<K, V>::get(K key) {
+  MDB_val c_key, c_val;
+  MDB_txn *txn;
+
+
+  c_key.mv_size = sizeof(K);
+  c_key.mv_data = (&key);
+
+
+  mdb_txn_begin(env, NULL, 0, &txn);
+  mdb_dbi_open(txn, NULL, 0, &dbi);
+
+  mdb_get(txn, dbi, &c_key, &c_val);
+
+  mdb_txn_abort(txn);
+  return *(V *)c_val.mv_data;
+  // return 1;
 }
 
 template <typename K, typename V>
-V btree_index::get(K&& key) {
-    MDB_val c_key, c_val;
-    MDB_txn *txn;
+void btree_index<K, V>::insert(K key, V val) {
+  MDB_val c_key, c_val;
+  MDB_txn *txn;
+  int res = 0;
 
-    c_key.mv_size = sizeof(K);
-    c_key.mv_data = reinterpret_cast<void*>(&key);
 
-    mdb_txn_begin(env, NULL, 0, &txn);
-    mdb_dbi_open(txn, NULL, 0, &dbi);
+  res = mdb_txn_begin(env, NULL, 0, &txn);
 
-    mdb_get(txn, dbi, &c_key, &c_val);
+  res = mdb_dbi_open(txn, NULL, 0, &dbi);
 
-    mdb_txn_abort(txn);
-    return reinterpret_cast<V*>(&c_val.mv_data);
+
+  c_key.mv_size = sizeof(K);
+
+  c_key.mv_data = (&key);
+
+
+  c_val.mv_size = sizeof(V);
+  c_val.mv_data = (&val);
+
+  mdb_put(txn, dbi, &c_key, &c_val, 0);
+
+
+  mdb_txn_commit(txn);
+
+  // Check if it is in valid state
+  mdb_env_stat(env, &mst);
 }
 
 template <typename K, typename V>
-int btree_index::insert(K&& key, V&& val) {
-    MDB_val c_key, c_val;
-    MDB_txn *txn;
-
-
-    mdb_txn_begin(env, NULL, 0, &txn);
-    mdb_dbi_open(txn, NULL, 0, &dbi);
-
-
-    c_key.mv_size = sizeof(K);
-    c_key.mv_data = reinterpret_cast<void*>(&key);
-
-    c_val.mv_size = sizeof(V);
-    c_val.mv_data = reinterpret_cast<void*>(&val);
-
-    mdb_put(txn, dbi, &c_key, &c_val, MDB_NOOVERWRITE);
-
-    mdb_txn_commit(txn);
-    // Check is in valid state
-    mdb_env_stat(env, &mst);
-
-
+btree_index<K, V>::~btree_index() {
+  mdb_dbi_close(env, dbi);
+  mdb_env_close(env);
 }
 
-btree_index::~btree_index() {
-    mdb_dbi_close(env, dbi);
-    mdb_env_close(env);
-}
+}//namespace index
+}//namespace ametsuchi
