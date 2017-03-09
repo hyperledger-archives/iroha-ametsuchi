5a891a29b45f3e97a4634ae407f59cf387a082b6
kamilsa <kamilsa16@gmail.com>
2017-03-09 23:44:28 +0300

    Feature/file test (#34)
    * Field table header
    
    * put and get draft
    
    * iterator draft
    
    * [WIP] working on tests
    
    * simple test on put and get
    
    * working iterator
    
    * refactoring
    
    * yet another refactoring
    
    * Change interface of File
    
    * Fixes
    
    * Finished field_table
    
    * Read only file test
    
    * seek fix
    
    * slight refactoring
    
    * Base table header
    
    * Change API File and refactored FieldTable
    
    * Fixes test and refactored FixedTable for new File API
    
    * got rid of warning
    
    * benchmark file
    
    * benchmark file
    
    * filewrite benchmark
    
    * huge file write/read test
    
    * test fails
    
    * huge file write and read
    
    * refactor
    
    * huge file writing test
    
    * slight fix in benchmark
    
    * read file benchmark
    
    * slight fix

--- test/ametsuchi/file/file_test.cc
+++ test/ametsuchi/file/file_test.cc
@@ -19,12 +19,14 @@
 
 #include <ametsuchi/file/file.h>
 #include <ametsuchi/globals.h>
+#include <ametsuchi/serializer.h>
 
 namespace ametsuchi {
 namespace file {
 
+
 TEST(FileTest, ReadWriteFileTest) {
-  std::string   filename = "/tmp/test1";
+  std::string filename = "/tmp/test1";
   ReadWriteFile f(filename);
   f.remove();
   auto opened = f.open();
@@ -65,10 +67,95 @@ TEST(FileTest, ReadWriteFileTest) {
   ASSERT_EQ(ByteArray({0xfe, 0xfa, 0xfe, 3, 4, 3, 4, 0xff, 0xff, 0xff}), res)
       << "wrong answer";
 
+  // testing seek
+  for (uint32_t i = 0; i < f.size(); i++) {
+    f.seek(i);
+    ASSERT_EQ(i, f.position());
+  }
+
+  f.seek(0);
+  res = f.read(3);
+  ASSERT_EQ(ByteArray({0xfe, 0xfa, 0xfe}), res);
+
+  f.seek_to_end();
+  ASSERT_EQ(f.position(), f.size());
+
+  ASSERT_TRUE(f.can_write());
+  ASSERT_TRUE(f.can_read());
+
+
   f.close();
 }
 
+TEST(FileTest, ReadOnlyFileTest) {
+  std::string filename = "/tmp/test1";
 
+  ReadWriteFile rwf(filename);
+  rwf.remove();
+  rwf.open();
+  rwf.write(ByteArray({0xfe, 0xfa, 0xfe, 3, 4, 3, 4, 0xff, 0xff, 0xff}));
+  rwf.close();
+
+  ReadOnlyFile f(filename);
+
+  ASSERT_TRUE(f.can_read());
+  ASSERT_FALSE(f.can_write());
+  ASSERT_FALSE(f.is_opened());
+
+  auto opened = f.open();
+
+  ASSERT_TRUE(opened);
+  ASSERT_TRUE(f.can_read());
+
+  ByteArray res = f.read(3);
+  ASSERT_EQ(ByteArray({0xfe, 0xfa, 0xfe}), res);
+
+  ASSERT_EQ(f.position(), 3);
+
+  res = f.read(4);
+  ASSERT_EQ(ByteArray({3, 4, 3, 4}), res);
+  ASSERT_EQ(f.position(), 7);
+
+  f.seek(1);
+  res = f.read(2);
+  ASSERT_EQ(ByteArray({0xfa, 0xfe}), res);
+
+  f.close();
+}
+
+TEST(FileTest, HugeFileWriteRead) {
+  size_t size = 10000000;
+
+  std::string filename = "/tmp/test1";
+  ReadWriteFile writeFile(filename);
+  writeFile.remove();
+
+  if (writeFile.open()) {
+    for (uint32_t i = 0; i < size; i++) {
+      ByteArray memory(4);
+      uint8_t *ptr = memory.data();
+      PUT_UINT(ptr, i, uint32_t);
+      uint32_t a = 0;
+      GET_UINT(&a, ptr, uint32_t);
+      writeFile.append(memory);
+    }
+  }
+  writeFile.close();
+
+  ReadOnlyFile readFile(filename);
+  if (readFile.open()) {
+    for (uint32_t i = 0; i < size; i++) {
+      ByteArray memory = readFile.read(4);
+      uint32_t res;
+      uint8_t *ptr = memory.data();
+      //      std::cout << readFile.size() << std::endl; // if this is
+      //      uncommented no failure occurs:)
+      GET_UINT(&res, ptr, uint32_t);
+      ASSERT_EQ(i, res);
+    }
+  }
+  readFile.close();
+}
 
 /*
 TEST_F(FileTest, PositionTest) {
