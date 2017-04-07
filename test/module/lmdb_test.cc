f0d409a3814e592824f8cc1ff2b79bb894f38cbb
Bulat Nasrulin <bulat.nasrulin@gmail.com>
2017-04-07 19:13:58 +0300

    Test fixed

--- test/module/lmdb_test.cc
+++ test/module/lmdb_test.cc
@@ -83,68 +83,12 @@ class LMDB_Test : public ::testing::Test {
   MDB_dbi dbi_index1;
   MDB_cursor *cursor_1;
 };
-/*
-TEST_F(LMDB_Test, AppendSameKeyAndRead) {
-  int res;
+
+
+TEST_F(LMDB_Test, LMDB_Dup_update){
+
   open_tx();
 
-  // WRITE
-  MDB_val key;
-  key.mv_data = (void *)"helloworld";
-  key.mv_size = 10;
-
-  for (int i = 0; i < 255; i++) {
-    MDB_val val;
-
-    val.mv_data = &i;
-    val.mv_size = sizeof(int);
-
-    HANDLE(mdb_cursor_put(cursor_1, &key, &val, 0));
-  }
-
-  key.mv_data = (void *)"hell0world";
-  key.mv_size = 10;
-
-  for (int i = 0; i < 255; i++) {
-    MDB_val val;
-
-    val.mv_data = &i;
-    val.mv_size = sizeof(int);
-
-    HANDLE(mdb_cursor_put(cursor_1, &key, &val, 0));
-  }
-
-  commit_tx();
-
-  // READ
-  MDB_val val;
-  MDB_txn *read_tx;
-  MDB_dbi read_dbi;
-  MDB_cursor *cursor;
-
-  HANDLE(mdb_txn_begin(env, NULL, MDB_RDONLY, &read_tx));
-  HANDLE(mdb_dbi_open(read_tx, "TEST1", MDB_DUPSORT | MDB_DUPFIXED, &read_dbi));
-  HANDLE(mdb_cursor_open(read_tx, read_dbi, &cursor));
-  HANDLE(mdb_cursor_get(cursor, &key, &val, MDB_SET));
-
-  int result = 0;
-  int res1;
-  do {
-    int index = *reinterpret_cast<int *>(val.mv_data);
-    ASSERT_EQ(index, result++);
-    ((res1 = mdb_cursor_get(cursor, &key, &val, MDB_NEXT_DUP)));
-  } while (res1 == 0);
-
-  mdb_cursor_close(cursor);
-  mdb_txn_abort(read_tx);
-  ASSERT_EQ(result, 255);
-}
-*/
-TEST_F(LMDB_Test, LMDB_update){
-  int res;
-  open_tx();
-
-  // WRITE
   MDB_val key;
   key.mv_data = (void *)"helloworld";
   key.mv_size = 10;
@@ -153,57 +97,58 @@ TEST_F(LMDB_Test, LMDB_update){
 
   int a = 2;
   val1.mv_data = &a;
-  val1.mv_size = sizeof(int);
+  val1.mv_size = sizeof(a);
 
   HANDLE(mdb_cursor_put(cursor_1, &key, &val1, 0));
 
   MDB_val val2;
   int b = 3;
-  val2.mv_data = &a;
-  val2.mv_size = sizeof(int);
+  val2.mv_data = &b;
+  val2.mv_size = sizeof(b);
 
   HANDLE(mdb_cursor_put(cursor_1, &key, &val2, 0));
   commit_tx();
 
   // retrive
   MDB_val val;
+
+  open_tx();
+
+  // We want to replace element that has key and value val1
+  HANDLE(mdb_cursor_get(cursor_1, &key, &val1, MDB_GET_BOTH));
+
+  auto a_retrived = *reinterpret_cast<int *>(val1.mv_data);
+  ASSERT_EQ(a, a_retrived);
+
+  // Update current data:
+  int new_a = 5;
+  MDB_val new_val1;
+  new_val1.mv_data = &new_a;
+  new_val1.mv_size = sizeof(new_a);
+
+  // Remove current data
+  HANDLE(mdb_cursor_del(cursor_1, 0));
+  // Put a new value, and sort it
+  HANDLE(mdb_cursor_put(cursor_1, &key, &new_val1, 0));
+
+  commit_tx();
+
+
+  // Read only transaction:
   MDB_txn *read_tx;
   MDB_dbi read_dbi;
   MDB_cursor *read_cursor;
 
   HANDLE(mdb_txn_begin(env, NULL, MDB_RDONLY, &read_tx));
   HANDLE(mdb_dbi_open(read_tx, "TEST1", MDB_DUPSORT | MDB_DUPFIXED, &read_dbi));
+
   HANDLE(mdb_cursor_open(read_tx, read_dbi, &read_cursor));
   HANDLE(mdb_cursor_get(read_cursor, &key, &val, MDB_SET));
-
-  int a_retrived = *reinterpret_cast<int *>(val.mv_data);
-  ASSERT_EQ(a, a_retrived);
-  mdb_cursor_close(read_cursor);
-  mdb_dbi_close(env, read_dbi);
-
-  // delete key value pair
-  MDB_txn *del_tx;
-  MDB_dbi del_dbi;
-//  MDB_cursor *del_cursor;
-  HANDLE(mdb_txn_begin(env, NULL, 0, &del_tx));
-  HANDLE(mdb_dbi_open(del_tx, "TEST1", MDB_DUPSORT | MDB_DUPFIXED, &del_dbi));
-//  HANDLE(mdb_cursor_open(del_tx, del_dbi, &del_cursor));
-
-  HANDLE(mdb_del(del_tx, del_dbi, &key, &val));
-  HANDLE(mdb_txn_commit(del_tx));
-//  mdb_cursor_close(del_cursor);
-  mdb_dbi_close(env, del_dbi);
-
-  // check if record deleted
-  MDB_txn *read_tx_again;
-  MDB_dbi read_dbi_again;
-  MDB_cursor *cursor_again;
-  HANDLE(mdb_txn_begin(env, NULL, 0, &read_tx_again));
-  HANDLE(mdb_dbi_open(read_tx_again, "TEST1", MDB_DUPSORT | MDB_DUPFIXED, &read_dbi_again));
-  HANDLE(mdb_cursor_open(read_tx_again, read_dbi_again, &cursor_again));
-  HANDLE(mdb_cursor_get(cursor_again, &key, &val, MDB_SET));
-
   a_retrived = *reinterpret_cast<int *>(val.mv_data);
-  ASSERT_NE(a, a_retrived);
+  ASSERT_EQ(b, a_retrived);
 
+  HANDLE(mdb_cursor_get(read_cursor, &key, &val, MDB_NEXT_DUP));
+  a_retrived = *reinterpret_cast<int *>(val.mv_data);
+
+  ASSERT_EQ(new_a, a_retrived);
 }
