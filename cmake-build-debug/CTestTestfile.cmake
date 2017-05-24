16f6fda7864d0a3a16d3d1915a0e7ed470058131
Andrei Lebedev <lebdron@gmail.com>
2017-05-24 13:06:58 +0300

    Remove build folder

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kamilsa/dev/iroha-ametsuchi
-# Build directory: /home/kamilsa/dev/iroha-ametsuchi/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(test)
--- cmake-build-debug/test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kamilsa/dev/iroha-ametsuchi/test
-# Build directory: /home/kamilsa/dev/iroha-ametsuchi/cmake-build-debug/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(wsv_redis_test "/home/kamilsa/dev/iroha-ametsuchi/cmake-build-debug/test_bin/wsv_redis_test")
