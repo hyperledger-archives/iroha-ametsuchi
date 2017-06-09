1dab630dff69644d7c2698aa2ac94fd8d25acbd5
Bogdan Vaneev <warchantua@gmail.com>
2017-06-09 19:10:08 +0300

    Added docker test file

---
+++ scripts/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+
+docker-compose down 1>/dev/null 2>&1 || true
+docker-compose -f docker-compose-test.yml up -d || exit 1
+
+export PGHOST=192.168.55.2
+export PGUSER=zekret
+export PGPASSWORD=secretpasswd
+export REDISHOST=192.168.55.3
+
+echo "Wait 30 sec.." && sleep 30
+
+cmake -Bbuild -H..
+cd build && make -j10 && ctest
